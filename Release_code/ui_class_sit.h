46c5400e349da24cc6e29e7adcdc369a1ba734d3
fbqweasd <fbqweasd2911@gmail.com>
2018-05-20 00:46:53 +0900

    code update

---
+++ Release_code/ui_class_sit.h
@@ -0,0 +1,258 @@
+/********************************************************************************
+** Form generated from reading UI file 'class_sit.ui'
+**
+** Created by: Qt User Interface Compiler version 5.10.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_CLASS_SIT_H
+#define UI_CLASS_SIT_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Class_sit
+{
+public:
+    QPushButton *sit_set_BT;
+    QWidget *layoutWidget;
+    QHBoxLayout *G2;
+    QVBoxLayout *Line_3;
+    QPushButton *Sit_3;
+    QPushButton *Sit_7;
+    QPushButton *Sit_11;
+    QPushButton *Sit_15;
+    QPushButton *Sit_19;
+    QVBoxLayout *Line_4;
+    QPushButton *Sit_4;
+    QPushButton *Sit_8;
+    QPushButton *Sit_12;
+    QPushButton *Sit_16;
+    QPushButton *Sit_20;
+    QWidget *layoutWidget_2;
+    QHBoxLayout *G1;
+    QVBoxLayout *Line_1;
+    QPushButton *Sit_1;
+    QPushButton *Sit_5;
+    QPushButton *Sit_9;
+    QPushButton *Sit_13;
+    QPushButton *Sit_17;
+    QVBoxLayout *Line_2;
+    QPushButton *Sit_2;
+    QPushButton *Sit_6;
+    QPushButton *Sit_10;
+    QPushButton *Sit_14;
+    QPushButton *Sit_18;
+    QPushButton *name_num_BT;
+
+    void setupUi(QWidget *Class_sit)
+    {
+        if (Class_sit->objectName().isEmpty())
+            Class_sit->setObjectName(QStringLiteral("Class_sit"));
+        Class_sit->resize(800, 700);
+        QFont font;
+        font.setFamily(QString::fromUtf8("\353\247\221\354\235\200 \352\263\240\353\224\225"));
+        font.setPointSize(10);
+        Class_sit->setFont(font);
+        sit_set_BT = new QPushButton(Class_sit);
+        sit_set_BT->setObjectName(QStringLiteral("sit_set_BT"));
+        sit_set_BT->setGeometry(QRect(250, 30, 250, 100));
+        QFont font1;
+        font1.setPointSize(12);
+        sit_set_BT->setFont(font1);
+        layoutWidget = new QWidget(Class_sit);
+        layoutWidget->setObjectName(QStringLiteral("layoutWidget"));
+        layoutWidget->setGeometry(QRect(410, 160, 371, 361));
+        G2 = new QHBoxLayout(layoutWidget);
+        G2->setSpacing(6);
+        G2->setContentsMargins(11, 11, 11, 11);
+        G2->setObjectName(QStringLiteral("G2"));
+        G2->setContentsMargins(0, 0, 0, 0);
+        Line_3 = new QVBoxLayout();
+        Line_3->setSpacing(6);
+        Line_3->setObjectName(QStringLiteral("Line_3"));
+        Sit_3 = new QPushButton(layoutWidget);
+        Sit_3->setObjectName(QStringLiteral("Sit_3"));
+
+        Line_3->addWidget(Sit_3);
+
+        Sit_7 = new QPushButton(layoutWidget);
+        Sit_7->setObjectName(QStringLiteral("Sit_7"));
+
+        Line_3->addWidget(Sit_7);
+
+        Sit_11 = new QPushButton(layoutWidget);
+        Sit_11->setObjectName(QStringLiteral("Sit_11"));
+
+        Line_3->addWidget(Sit_11);
+
+        Sit_15 = new QPushButton(layoutWidget);
+        Sit_15->setObjectName(QStringLiteral("Sit_15"));
+
+        Line_3->addWidget(Sit_15);
+
+        Sit_19 = new QPushButton(layoutWidget);
+        Sit_19->setObjectName(QStringLiteral("Sit_19"));
+
+        Line_3->addWidget(Sit_19);
+
+
+        G2->addLayout(Line_3);
+
+        Line_4 = new QVBoxLayout();
+        Line_4->setSpacing(6);
+        Line_4->setObjectName(QStringLiteral("Line_4"));
+        Sit_4 = new QPushButton(layoutWidget);
+        Sit_4->setObjectName(QStringLiteral("Sit_4"));
+
+        Line_4->addWidget(Sit_4);
+
+        Sit_8 = new QPushButton(layoutWidget);
+        Sit_8->setObjectName(QStringLiteral("Sit_8"));
+
+        Line_4->addWidget(Sit_8);
+
+        Sit_12 = new QPushButton(layoutWidget);
+        Sit_12->setObjectName(QStringLiteral("Sit_12"));
+
+        Line_4->addWidget(Sit_12);
+
+        Sit_16 = new QPushButton(layoutWidget);
+        Sit_16->setObjectName(QStringLiteral("Sit_16"));
+
+        Line_4->addWidget(Sit_16);
+
+        Sit_20 = new QPushButton(layoutWidget);
+        Sit_20->setObjectName(QStringLiteral("Sit_20"));
+
+        Line_4->addWidget(Sit_20);
+
+
+        G2->addLayout(Line_4);
+
+        layoutWidget_2 = new QWidget(Class_sit);
+        layoutWidget_2->setObjectName(QStringLiteral("layoutWidget_2"));
+        layoutWidget_2->setGeometry(QRect(10, 160, 371, 361));
+        G1 = new QHBoxLayout(layoutWidget_2);
+        G1->setSpacing(6);
+        G1->setContentsMargins(11, 11, 11, 11);
+        G1->setObjectName(QStringLiteral("G1"));
+        G1->setContentsMargins(0, 0, 0, 0);
+        Line_1 = new QVBoxLayout();
+        Line_1->setSpacing(6);
+        Line_1->setObjectName(QStringLiteral("Line_1"));
+        Sit_1 = new QPushButton(layoutWidget_2);
+        Sit_1->setObjectName(QStringLiteral("Sit_1"));
+
+        Line_1->addWidget(Sit_1);
+
+        Sit_5 = new QPushButton(layoutWidget_2);
+        Sit_5->setObjectName(QStringLiteral("Sit_5"));
+
+        Line_1->addWidget(Sit_5);
+
+        Sit_9 = new QPushButton(layoutWidget_2);
+        Sit_9->setObjectName(QStringLiteral("Sit_9"));
+
+        Line_1->addWidget(Sit_9);
+
+        Sit_13 = new QPushButton(layoutWidget_2);
+        Sit_13->setObjectName(QStringLiteral("Sit_13"));
+
+        Line_1->addWidget(Sit_13);
+
+        Sit_17 = new QPushButton(layoutWidget_2);
+        Sit_17->setObjectName(QStringLiteral("Sit_17"));
+
+        Line_1->addWidget(Sit_17);
+
+
+        G1->addLayout(Line_1);
+
+        Line_2 = new QVBoxLayout();
+        Line_2->setSpacing(6);
+        Line_2->setObjectName(QStringLiteral("Line_2"));
+        Sit_2 = new QPushButton(layoutWidget_2);
+        Sit_2->setObjectName(QStringLiteral("Sit_2"));
+
+        Line_2->addWidget(Sit_2);
+
+        Sit_6 = new QPushButton(layoutWidget_2);
+        Sit_6->setObjectName(QStringLiteral("Sit_6"));
+
+        Line_2->addWidget(Sit_6);
+
+        Sit_10 = new QPushButton(layoutWidget_2);
+        Sit_10->setObjectName(QStringLiteral("Sit_10"));
+
+        Line_2->addWidget(Sit_10);
+
+        Sit_14 = new QPushButton(layoutWidget_2);
+        Sit_14->setObjectName(QStringLiteral("Sit_14"));
+
+        Line_2->addWidget(Sit_14);
+
+        Sit_18 = new QPushButton(layoutWidget_2);
+        Sit_18->setObjectName(QStringLiteral("Sit_18"));
+
+        Line_2->addWidget(Sit_18);
+
+
+        G1->addLayout(Line_2);
+
+        name_num_BT = new QPushButton(Class_sit);
+        name_num_BT->setObjectName(QStringLiteral("name_num_BT"));
+        name_num_BT->setGeometry(QRect(100, 550, 181, 81));
+        name_num_BT->setFont(font1);
+
+        retranslateUi(Class_sit);
+
+        QMetaObject::connectSlotsByName(Class_sit);
+    } // setupUi
+
+    void retranslateUi(QWidget *Class_sit)
+    {
+        Class_sit->setWindowTitle(QApplication::translate("Class_sit", "Class_sit", nullptr));
+        sit_set_BT->setText(QApplication::translate("Class_sit", "\354\236\220\353\246\254 \353\275\221\352\270\260", nullptr));
+        Sit_3->setText(QString());
+        Sit_7->setText(QString());
+        Sit_11->setText(QString());
+        Sit_15->setText(QString());
+        Sit_19->setText(QString());
+        Sit_4->setText(QString());
+        Sit_8->setText(QString());
+        Sit_12->setText(QString());
+        Sit_16->setText(QString());
+        Sit_20->setText(QString());
+        Sit_1->setText(QString());
+        Sit_5->setText(QString());
+        Sit_9->setText(QString());
+        Sit_13->setText(QString());
+        Sit_17->setText(QString());
+        Sit_2->setText(QString());
+        Sit_6->setText(QString());
+        Sit_10->setText(QString());
+        Sit_14->setText(QString());
+        Sit_18->setText(QString());
+        name_num_BT->setText(QApplication::translate("Class_sit", "\354\235\264\353\246\204\354\234\274\353\241\234 \353\263\264\352\270\260", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Class_sit: public Ui_Class_sit {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_CLASS_SIT_H
